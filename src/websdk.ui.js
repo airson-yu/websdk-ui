9f626f89dfa67ea89e801c06384cf62c066ea88c
yurh <airson_yu@163.com>
2020-08-05 16:22:08 +0800

    ws connection retry fix

--- src/websdk.ui.js
+++ src/websdk.ui.js
@@ -12,7 +12,7 @@ class websdkui {
             login_user: {},
         };
         this.configApi = new configApi();//配置对象
-        logger.debug('websdk_ui_version: 2020.08.04.01');
+        logger.debug('websdk_ui_version: 2020.08.05.01');
     }
 
     demo = () => {
