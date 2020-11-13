03aa3209845b45cf8551c60747db3f31a40d5100
airson <airson_yu@163.com>
2020-11-13 14:33:07 +0800

    websdk: R08.00.37,date: 20201113

--- src/websdk.ui.js
+++ src/websdk.ui.js
@@ -12,7 +12,7 @@ class websdkui {
             login_user: {},
         };
         this.configApi = new configApi();//配置对象
-        logger.debug('websdk_ui_version: R08.00.37,date: 20201112');
+        logger.debug('websdk_ui_version: R08.00.37,date: 20201113');
     }
 
     demo = () => {
